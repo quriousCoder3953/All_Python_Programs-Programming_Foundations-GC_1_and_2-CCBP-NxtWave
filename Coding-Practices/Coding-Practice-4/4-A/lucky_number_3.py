6c3d7634d93f7220024a3ae8bc2d0b67420c252c
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-18 17:53:33 +0530

    Update lucky_number_3.py

--- Coding-Practices/Coding-Practice-4/4-A/lucky_number_3.py
+++ Coding-Practices/Coding-Practice-4/4-A/lucky_number_3.py
@@ -1,1 +1,10 @@
+A = int(input())
 
+if A % 6 ==0:
+    print("Number is divisible by 6")
+elif A % 3 == 0:
+    print("Number is divisible by 3")
+elif A % 2 == 0:
+    print("Number is divisible by 2")
+else:
+    print("Number is not divisible by 2, 3 or 6")
