3e457a0dcfacec5ad6ef7318df5aabb9ca2096d6
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-18 17:57:45 +0530

    Update special_number.py

--- Coding-Practices/Coding-Practice-4/4-A/special_number.py
+++ Coding-Practices/Coding-Practice-4/4-A/special_number.py
@@ -1,1 +1,13 @@
+A = input()
 
+X = (int(A[0]) + int(A[1])) == 7
+Y = (int(A[0]) == 7) or (int(A[1]) == 7)
+
+A = int(A)
+
+Z = A % 7 == 0
+
+if X or Y or Z:
+    print("Special Number")
+else:
+    print("Normal Number")
