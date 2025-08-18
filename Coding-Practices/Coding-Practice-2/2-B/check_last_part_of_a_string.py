57027c0c73aa3134fd77801363ad9005593b93fc
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-18 12:43:03 +0530

    Update check_last_part_of_a_string.py

--- Coding-Practices/Coding-Practice-2/2-B/check_last_part_of_a_string.py
+++ Coding-Practices/Coding-Practice-2/2-B/check_last_part_of_a_string.py
@@ -1,1 +1,11 @@
-
+A = input()
+B = input()
+L = len(B)
+z = ""
+for i in A:
+    z = i + z
+last_part = z[:L]
+last_part_of_string = ""
+for i in last_part:
+    last_part_of_string = i + last_part_of_string
+print(last_part_of_string == B)
