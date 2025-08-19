0e077a12078c9f7d3003af111a51ea0dbd2e2eb7
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-19 17:06:39 +0530

    Create get_season.py

---
+++ Assignments/Assignment-5/get_season.py
@@ -0,0 +1,12 @@
+M = int(input())
+
+if (M == 11) or (M ==12) or (M == 1):
+    print("Winter")
+elif (M == 2) or (M == 3):
+    print("Spring")
+elif (M == 4) or (M == 5) or (M == 6):
+    print("Summer")
+elif (M == 7) or (M == 8):
+    print("Rainy")
+else:
+    print("Autumn")
