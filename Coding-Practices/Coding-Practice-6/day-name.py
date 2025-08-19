0162dccb3710dc3544abcce7f98a94f63b09525e
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-19 18:34:35 +0530

    Update and rename Coding-Practices/Coding-Practice-6/6-A/.gitkeep to Coding-Practices/Coding-Practice-6/day-name.py

---
+++ Coding-Practices/Coding-Practice-6/day-name.py
@@ -0,0 +1,16 @@
+D = input()
+
+if D == '1':
+    print("Monday")
+elif D == '2':
+    print("Tuesday")
+elif D == '3':
+    print("Wednesday")
+elif D == '4':
+    print("Thursday")
+elif D == '5':
+    print("Friday")
+elif D == '6':
+    print("Saturday")
+else:
+    print("Sunday")
