de58517d2c6d7d50257983ba4cd811729b4c48b7
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-19 17:00:22 +0530

    Create not_divisible_by_numbers.py

---
+++ Assignments/Assignment-4/not_divisible_by_numbers.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if A % 2 == 0:
+    print("False")
+elif A % 3 == 0:
+    print("False")
+elif A % 5 == 0:
+    print("False")
+elif A % 7 == 0:
+    print("False")
+else:
+    print("True")
