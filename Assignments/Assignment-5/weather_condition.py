4c352cd8977fd48ff1a95e0854b2db672c25d9c5
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-19 17:07:55 +0530

    Create weather_condition.py

---
+++ Assignments/Assignment-5/weather_condition.py
@@ -0,0 +1,14 @@
+T = float(input())
+
+if T<0:
+    print("Freezing weather")
+elif 0 <= T < 10:
+    print("Very Cold weather")
+elif 10 <= T < 20:
+    print("Cold weather")
+elif 20 <= T <30:
+    print("Normal")
+elif 30 <= T <40:
+    print("Hot")
+else:
+    print("Very Hot")
