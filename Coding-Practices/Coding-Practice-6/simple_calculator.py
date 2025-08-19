18e6e6ab73b345875fad65e881409c5ae31cc09d
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-19 18:36:32 +0530

    Create simple_calculator.py

---
+++ Coding-Practices/Coding-Practice-6/simple_calculator.py
@@ -0,0 +1,15 @@
+O = input()
+
+A = int(input())
+B = int(input())
+
+if O == "+":
+    print(A + B)
+elif O == "-":
+    print(A - B)
+elif O == "*":
+    print(A * B)
+elif O == "/":
+    print(A / B)
+else:
+    print(A % B)
