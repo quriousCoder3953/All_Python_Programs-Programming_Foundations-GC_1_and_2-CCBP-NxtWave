bc87737123d3d4c41991c92e264bf5a7e9e6816c
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-08-20 12:20:36 +0530

    Create right_angled_triangle_3.py

---
+++ Coding-Practices/Coding-Practice-7/7-B/right_angled_triangle_3.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+for i in range(1, N+1):
+    if i == N:
+        triangled_string = ("+" + " ") * i
+    else:
+        triangled_string = ("*" + " ") * i
+    print(triangled_string)
